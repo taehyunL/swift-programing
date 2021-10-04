97a7bf62725abf99bca8f377e5b09a9262c469d6
taehyunL <taylor1233@naver.com>
2021-10-04 20:48:53 +0900

    클로저를 통한 프로퍼티 기본값 설정

---
+++ ch11/ch11/11-11,12.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+struct Student {
+    var name: String?
+    var number: Int?
+}
+
+class SchoolClass {
+    var students: [Student] = {
+        var arr: [Student] = [Student]()
+        
+        for num in 1...15 {
+            var student: Student = Student(name: nil, number: num)
+            arr.append(student)
+        }
+        
+        return arr
+    }()
+}
+
+let myClass: SchoolClass = SchoolClass()
+print(myClass.students.count)
