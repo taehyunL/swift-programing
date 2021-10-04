1e6c8f78a2ee5881fff2c68275c5012816dea7c4
taehyunL <taylor1233@naver.com>
2021-10-04 20:48:08 +0900

    실패 가능한 이니셜라이저

---
+++ ch11/ch11/11-9 .swift
@@ -0,0 +1,46 @@
+import Foundation
+
+class Person {
+    let name: String
+    var age: Int?
+    
+    init?(name: String) {   //실패 가능한 이니셜라이저
+        
+        if name.isEmpty {
+            return nil
+        }
+        self.name = name
+    }
+    
+    init?(name: String, age: Int) {
+        if name.isEmpty || age < 0 {
+            return nil
+        }
+        self.name = name
+        self.age = age
+    }
+}
+
+let yagom: Person? = Person(name: "yagom", age: 99)
+
+if let person: Person = yagom {
+    print(person.name)
+} else {
+    print("Person wasn't initialized")
+}
+
+let chope: Person? = Person(name: "chope", age: -10)
+
+if let person: Person = chope {
+    print(person.name)
+} else {
+    print("Person wasn't initialized")
+}
+
+let eric: Person? = Person(name: "", age: 30)
+
+if let person: Person = eric {
+    print(person.name)
+} else {
+    print("Person wasn't initialized")
+}
