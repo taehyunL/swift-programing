4a3994f32be13f05ee08895900015f2acc923e60
taehyunL <taylor1233@naver.com>
2021-10-27 15:23:58 +0900

    public 과 open

---
+++ ch12/ch12/12-1,2.swift
@@ -0,0 +1,15 @@
+//import Foundation
+
+// 스위프트 표준 라이브러리에 정의되어 있는 Bool 타입
+//public struct Bool {
+//    public init()
+//}
+
+//Foundation 프레임워크에 정의되어 있는 개방 접근수준의 NSString 클래스
+//open class NSString : NSObject, NSCopying, NSMutableCopying, NSSecureCoding {
+//    open var length: Int { get }
+//    open func character(at index: Int) -> unichar
+//    public init()
+//    public init?(coder aDecoder: NSCoder)
+//}
+
